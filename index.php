a293b3dc9a85246698f761b8a49e6dd9a11ded60
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-03-13 21:43:31 +0200

    updated core login file link

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <div class="wrapper">
     <h1>Login</h1>
     <p id="error-message"></p>
-    <form id="form" action="php/auth-reg.php" method="POST">
+    <form id="form" action="php/login.php" method="POST">
       <div>
         <label for="username-input">
           <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></span>
